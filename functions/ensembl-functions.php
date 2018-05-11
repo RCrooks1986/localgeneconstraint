1dfc68603f597a67ec2f9fd88c4e5fa1a48fe824
Richard Crooks <email@rocrooks.co.uk>
2018-05-11 17:34:48 +0100

    Initiate functions for retrieving data from ensembl database

---
+++ functions/ensembl-functions.php
@@ -0,0 +1,51 @@
+<?php
+include_once 'api-functions.php';
+//---FunctionBreak---
+/*Turns a gene symbol into an array with gene sequence and sequence names which can be used to appropriately query databases
+
+$id is the gene symbol (e.g. BRCA1)
+
+Output is an array in the format array("Symbol"=>"","ENSG"=>,"LRG"=>)*/
+//---DocumentationBreak---
+function getsequenceids($id)
+	{
+	//Get sequence identifiers in JSON format
+	$json = getrawdatafromapi($id,"EnsemblID");
+	$json = json_decode($json,true);
+	
+	$output = array("Symbol"=>$id);
+	
+	foreach($json as $identifier)
+		{
+		//Check that ID is set
+		if (isset($identifier['id']) == true)
+			{
+			//Determine if ENSG number or LRG number
+			if (strpos($identifier['id'],"ENSG") !== false)
+				$output['ENSG'] = $identifier['id'];
+			elseif (strpos($identifier['id'],"LRG") !== false)
+				$output['LRG'] = $identifier['id'];
+			}
+		}
+	
+	Return $output;
+	}
+//---FunctionBreak---
+/*Gets a full length sequence from Ensembl
+
+$id is either the ENSG number, or an array containing it identified by ['ENSG']
+
+Output is the sequence as plain text*/
+//---DocumentationBreak---
+function getensemblsequence($id)
+	{
+	//Get ENSG ID from array if needed
+	if (is_array($id) == true)
+		$id = $id['ENSG'];
+	
+	$sequence = getrawdatafromapi($id,"EnsemblSequence");
+	
+	Return $sequence;
+	}
+//---FunctionBreak---
+?>
