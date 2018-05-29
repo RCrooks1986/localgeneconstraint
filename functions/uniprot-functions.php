81c1915cbedac384b8708bad69557f58c184ecd9
Richard Crooks <email@rocrooks.co.uk>
2018-05-29 09:53:38 +0100

    Functions to retrieve domain information from UniProt

--- functions/uniprot-functions.php
+++ functions/uniprot-functions.php
@@ -1,18 +1,46 @@
 <?php
 include_once 'api-functions.php';
 //---FunctionBreak---
-/*Turn a ENTS transcript ID into a UniProtKB ID
+/*Get sequence and domain information from UniProt using a gene symbol
 
-$id is the ENTS transcript identifier
+$id is the Gene symbol
 
-Output is the UniProt accession number*/
+Output is an array with UniProt ['ID'], ['Sequence'] and ['Domains']*/
 //---DocumentationBreak---
-function getuniprotaccession($id)
+function getuniprotdetails($id)
 	{
-	$id = "Ensembl:" . $id;
-	$uniprotkb = getrawdatafromapi($id,"UniProtAccession");
+	//Retrieve JSON data from UniProt API
+	$uniprotkb = getrawdatafromapi($id,"UniProtDetails");
 	$uniprotkb = json_decode($uniprotkb,true);
-	print_r($uniprotkb);
+	$uniprotkb = $uniprotkb[0];
+	
+	//Get UniProt ID
+	$uniprotid = $uniprotkb['accession'];
+	
+	//Get Amino Acid Sequence
+	$sequence = $uniprotkb['sequence']['sequence'];
+	
+	//Get sequence features and domains
+	$features = $uniprotkb['features'];
+	$featureslist = array();	
+	foreach ($uniprotkb['features'] as $feature)
+		{
+		//Move feature information to new array and add to output array
+		$featureoutput = array();
+		$featureoutput['type'] = $feature['type'];
+		$featureoutput['category'] = $feature['category'];
+		if (isset($feature['description']) == true)
+			$featureoutput['description'] = $feature['description'];
+		else
+			$featureoutput['description'] = "";
+		$featureoutput['start'] = $feature['begin'];
+		$featureoutput['end'] = $feature['end'];
+		
+		array_push($featureslist,$featureoutput);
+		}
+	
+	$output = array("UniProtID"=>$uniprotid,"Sequence"=>$sequence,"Features"=>$featureslist);
+	Return $output;
 	}
 //---FunctionBreak---
 ?>
