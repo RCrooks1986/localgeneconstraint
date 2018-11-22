f53e0b3bb4d6086b079b679df811fd3507d2e08b
Richard Crooks <email@rocrooks.co.uk>
2018-11-22 12:47:30 +0000

    Have a single variable to define the location of required function files

--- public/required-files.php
+++ public/required-files.php
@@ -1,11 +1,14 @@
 <?php
+//Directory where the required files are held
+$directory = "../functions/";
+
 //Include files containing shared functions which can also be used by other scripts
-include_once '../functions/api-functions.php';
-include_once '../functions/dna-functions.php';
-include_once '../functions/uniprot-functions.php';
-include_once '../functions/ensembl-functions.php';
-include_once '../functions/mutationrate-functions.php';
-include_once '../functions/exac-functions.php';
-include_once '../functions/bruteforce-functions.php';
-include_once '../functions/cycleloop-functions.php';
+include_once $directory . 'api-functions.php';
+include_once $directory . 'dna-functions.php';
+include_once $directory . 'uniprot-functions.php';
+include_once $directory . 'ensembl-functions.php';
+include_once $directory . 'mutationrate-functions.php';
+include_once $directory . 'exac-functions.php';
+include_once $directory . 'bruteforce-functions.php';
+include_once $directory . 'cycleloop-functions.php';
 ?>
