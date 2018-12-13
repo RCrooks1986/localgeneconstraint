39da96221b5132700d91b37a4682856533c55fed
Richard Crooks <email@rocrooks.co.uk>
2018-12-13 15:24:28 +0000

    Retrieve GeneSymbol and ENSG from list or API

---
+++ public/find-ids.php
@@ -0,0 +1,35 @@
+<?php
+$genesymbol = "JUN";
+
+if (isset($genesymbol) == false)
+  $genesymbol = "BRCA1";
+
+//Include API functions and the pre installed gene IDs
+include_once '../functions/api-functions.php';
+include_once 'gene-ids.php';
+
+if (isset($genelist[$genesymbol]) == true)
+  {
+  $ids = $genelist[$genesymbol];
+  }
+else
+  {
+  $json = getrawdatafromapi($genesymbol,"EnsemblID");
+  $json = json_decode($json,true);
+
+  $ids = array("GeneSymbol"=>$genesymbol);
+
+  foreach($json as $identifier)
+    {
+    //Check that ID is set
+    if (isset($identifier['id']) == true)
+      {
+      //Determine if ENSG number or LRG number
+      if (strpos($identifier['id'],"ENSG") !== false)
+        $ids['ENSG'] = $identifier['id'];
+      }
+    }
+  }
+
+print_r($ids);
+?>
